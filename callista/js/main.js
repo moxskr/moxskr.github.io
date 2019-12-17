0735ba59263e3fa3033e05b77c636895af1f647a
Max Skrypnyk <moxskr@gmail.com>
2019-12-17 19:48:29 +0200

    added calista

---
+++ callista/js/main.js
@@ -0,0 +1,26 @@
+const modal_window = document.getElementById('modal_window');
+const register_tab = document.getElementById('modal-register');
+const login_tab = document.getElementById('modal-login');
+
+function open_modal() {
+    modal_window.style.display = 'flex';
+}
+
+function close_modal() {
+    modal_window.style.display = 'none';
+}
+
+function open_register_tab() {
+    login_tab.style.display = 'none';
+    register_tab.style.display = 'block';
+    document.getElementById('modal-up').style.borderBottomColor = '#EB5757';
+    document.getElementById('modal-in').style.borderBottomColor = '#999';
+}
+
+function open_login_tab() {
+    register_tab.style.display = 'none';
+    login_tab.style.display = 'block';
+    document.getElementById('modal-in').style.borderBottomColor = '#EB5757';
+    document.getElementById('modal-up').style.borderBottomColor = '#999';
+}
+
